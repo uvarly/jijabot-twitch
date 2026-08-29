92678cd7076a634a89d1617c6cfc2f76908796cf
Filipp Stepanov <somerussianlad@gmail.com>
2026-08-29 14:58:54 +0300

    feat: add config logic

--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module jijabot
 
 go 1.25.4
 
-require github.com/gempir/go-twitch-irc/v4 v4.4.1
+require (
+	github.com/gempir/go-twitch-irc/v4 v4.4.1
+	github.com/go-playground/validator/v10 v10.30.3
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.52.0 // indirect
+	golang.org/x/sys v0.45.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,50 @@
 package config
 
-type Twitch struct {
-	Username string
-	Oauth    string
-	Channel  string
-}
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-playground/validator/v10"
+	"gopkg.in/yaml.v3"
+)
+
+const configPath = "config/config.yaml"
 
 type Config struct {
-	Twitch
+	Twitch TwitchConfig `yaml:"twitch"`
+}
+
+type TwitchConfig struct {
+	Username string `yaml:"username" validate:"required"`
+	Oauth    string `yaml:"oauth" validate:"required"`
+	Channel  string `yaml:"channel" validate:"required"`
 }
 
 func NewConfig() (Config, error) {
-	return Config{}, nil
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var cfg Config
+
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return Config{}, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	if err := cfg.validate(); err != nil {
+		return Config{}, fmt.Errorf("failed to validate config: %w", err)
+	}
+
+	return cfg, nil
+}
+
+func (c Config) validate() error {
+	validate := validator.New()
+
+	if err := validate.Struct(c); err != nil {
+		return err
+	}
+
+	return nil
 }
